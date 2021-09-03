2440ec5232ca78bc1bb25daf5fd434e3004f28ec
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-09-03 09:17:34 -0300

    Add new router system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import { DashboardPage } from "modules/admin/pages/Dashboard";
+import { App } from "modules/app/App";
 
-function App() {
+function Application() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/dashboard" component={DashboardPage} />
         <Redirect exact from="/" to="/dashboard" />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="" component={App} />
       </Switch>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default Application;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+import theme from "shared/styles/theme";
+import GlobalStyle from "shared/styles/globalStyle";
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme.main}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   document.getElementById("wrapper")
 );
