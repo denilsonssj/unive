1b64734efde08d81fb2d66e4e52ba99c3452ff2a
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-09-13 16:03:29 -0300

    Update not found page

--- frontend/src/modules/app/pages/NotFound/index.tsx
+++ frontend/src/modules/app/pages/NotFound/index.tsx
@@ -1,9 +1,12 @@
 import { useHistory } from "react-router";
 
+import notFoundImage from "assets/images/not_found.svg";
 import { 
   Container,
   Title,
   Subtitle,
+  Message,
+  Image,
   Button 
 } from "./styles";
 
@@ -11,16 +14,13 @@ export function NotFoundPage() {
 
   const history = useHistory();
 
-  function handleClickToBackInitialPageButton() {
-    history.push("/");
-    console.log(history);
-  }
-
   return (
     <Container>
+      <Image height="300" src={notFoundImage} alt="Not Found" />
       <Title>Error 404</Title>
-      <Subtitle>Page not Found</Subtitle>
-      <Button onClick={handleClickToBackInitialPageButton}>Back to inital page</Button>
+      <Subtitle>Page not found.</Subtitle>
+      <Message>The page you are looking for does not exist.</Message>
+      <Button onClick={() => history.push("/")}>Back to inital page</Button>
     </Container>
   );
 }
--- frontend/src/modules/app/pages/NotFound/styles.ts
+++ frontend/src/modules/app/pages/NotFound/styles.ts
@@ -11,14 +11,26 @@ export const Container = styled.div`
 
 export const Title = styled.h1`
   font-family: "Roboto", sans-serif;
-  font-size: 5.6rem;
-  font-style: italic;
+  font-size: 7.2rem;
   font-weight: 700;
+  margin: 2.0rem 0;
 `;
 
 export const Subtitle = styled.h3`
   font-family: "Roboto", sans-serif;
   font-size: 3.6rem;
+  font-weight: 500;
+  margin-bottom: 2.0rem;
+`;
+
+export const Message = styled.span`
+  font-size: 1.4rem;
+  font-weight: 400;
+  margin-bottom: 2.0rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
 `;
 
 export const Button = styled.button`
